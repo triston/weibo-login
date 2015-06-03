3efb58d71f786d8b58a7d438656bd20e893a17a2
Erick Guan <fantasticfears@gmail.com>
2015-06-03 11:49:31 +0800

    inline gem and bump version

--- plugin.rb
+++ plugin.rb
@@ -1,10 +1,79 @@
 # name: Weibo login
 # about: Authenticate with discourse with weibo.
-# version: 0.3.0
+# version: 0.4.0
 # author: Erick Guan
 
-gem 'omniauth-weibo-oauth2', '0.4.0'
+# inline gem 'omniauth-weibo-oauth2'
 
+require 'omniauth-oauth2'
+
+class OmniAuth::Strategies::Weibo < OmniAuth::Strategies::OAuth2
+  option :client_options, {
+                          :site           => "https://api.weibo.com",
+                          :authorize_url  => "/oauth2/authorize",
+                          :token_url      => "/oauth2/access_token"
+                        }
+  option :token_params, {
+                        :parse          => :json
+                      }
+
+  uid do
+    raw_info['id']
+  end
+
+  info do
+    {
+      :nickname     => raw_info['screen_name'],
+      :name         => raw_info['name'],
+      :location     => raw_info['location'],
+      :image        => find_image,
+      :description  => raw_info['description'],
+      :urls => {
+        'Blog'      => raw_info['url'],
+        'Weibo'     => raw_info['domain'].present?? "http://weibo.com/#{raw_info['domain']}" : "http://weibo.com/u/#{raw_info['id']}",
+      }
+    }
+  end
+
+  extra do
+    {
+      :raw_info => raw_info
+    }
+  end
+
+  def raw_info
+    access_token.options[:mode] = :query
+    access_token.options[:param_name] = 'access_token'
+    @uid ||= access_token.get('/2/account/get_uid.json').parsed["uid"]
+    @raw_info ||= access_token.get("/2/users/show.json", :params => {:uid => @uid}).parsed
+  end
+
+  def find_image
+    raw_info[%w(avatar_hd avatar_large profile_image_url).find { |e| raw_info[e].present? }]
+  end
+
+  ##
+  # You can pass +display+, +with_offical_account+ or +state+ params to the auth request, if
+  # you need to set them dynamically. You can also set these options
+  # in the OmniAuth config :authorize_params option.
+  #
+  # /auth/weibo?display=mobile&with_offical_account=1
+  #
+  def authorize_params
+    super.tap do |params|
+      %w[display with_offical_account forcelogin].each do |v|
+        if request.params[v]
+          params[v.to_sym] = request.params[v]
+        end
+      end
+    end
+  end
+
+end
+
+OmniAuth.config.add_camelization "weibo", "Weibo"
+
+# Discourse plugin
 class WeiboAuthenticator < ::Auth::Authenticator
 
   def name
